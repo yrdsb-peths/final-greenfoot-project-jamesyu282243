05ecc242c6b3569526efc44fc27f332e182f859d
jamesyu282243 <150282927+jamesyu282243@users.noreply.github.com>
2023-12-13 13:01:41 -0500

    added worm

--- MyWorld.java
+++ MyWorld.java
@@ -8,7 +8,7 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
  */
 public class MyWorld extends World
 {
-
+    Label titleLabel;
     /**
      * Constructor for objects of class MyWorld.
      * 
@@ -17,5 +17,20 @@ public class MyWorld extends World
     {    
         // Create a new world with 600x400 cells with a cell size of 1x1 pixels.
         super(600, 400, 1);
+        // Create the worm object
+        Worm worm = new Worm();
+        addObject(worm, 300, 300);
+
+        prepare();
+
+
+    }
+
+    /**
+     * Prepare the world for the start of the program.
+     * That is: create the initial objects and add them to the world.
+     */
+    private void prepare()
+    {
     }
 }
