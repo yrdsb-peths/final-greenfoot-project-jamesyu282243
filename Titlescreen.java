62aaed548d92fcca33b32a2fdf2d76870b6471a4
jamesyu282243 <150282927+jamesyu282243@users.noreply.github.com>
2024-01-15 13:02:15 -0500

    Update Titlescreen.java

--- Titlescreen.java
+++ Titlescreen.java
@@ -9,6 +9,9 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
 public class Titlescreen extends World
 {
     Label titleLabel = new Label("Elephant Invasion", 60);
+    Label startLabel = new Label("Press 'SPACE' to Start", 30);
+    Label instructionLabel = new Label("Objective: Eliminate All Elephants!", 30);
+
     /**
      * Constructor for objects of class Titlescreen.
      * 
@@ -17,10 +20,13 @@ public class Titlescreen extends World
     {    
         // Create a new world with 600x400 cells with a cell size of 1x1 pixels.
         super(600, 400, 1); 
-        
+
         addObject(titleLabel, getWidth()/2, getHeight()/2);
+        addObject(startLabel, getWidth()/2, getHeight()/2 + 120);
+        addObject(instructionLabel, getWidth()/2, getHeight() - 50);
+        prepare();
     }
-    
+
     /**
      * The main world act loop
      */
@@ -32,5 +38,13 @@ public class Titlescreen extends World
             MyWorld gameWorld = new MyWorld();
             Greenfoot.setWorld(gameWorld);
         } 
+    }
+    
+    /**
+     * Prepare the world for the start of the program.
+     * That is: create the initial objects and add them to the world.
+     */
+    private void prepare()
+    {
     }
 }
